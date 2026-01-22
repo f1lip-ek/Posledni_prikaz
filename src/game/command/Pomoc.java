7ded0c33c580e0b9d42f4cfccb900ccb788588ae
Filip <filipzitek@seznam.cz>
2026-01-22 21:25:45 +0100

    adding javaDocs to Pomoc.java

--- src/game/command/Pomoc.java
+++ src/game/command/Pomoc.java
@@ -7,8 +7,15 @@ import java.io.FileReader;
 import java.io.Reader;
 import java.util.Arrays;
 
+/**
+ * Trida pro command ktery vypisuje vsechny commandy ve hre
+ */
 public class Pomoc implements Command{
-
+    /**
+     * Metoda ktera precte pomoc.json ve kterem jsou napsane vsechny commandy ve hre
+     * @param text nic nedela
+     * @return textovy retezec ve kterem jsou napsane vsechny commandy ve hre
+     */
     @Override
     public String execute(String text) {
         Gson gson = new Gson();
@@ -23,6 +30,10 @@ public class Pomoc implements Command{
         return null;
     }
 
+    /**
+     * Metoda ktera rika jestli command ukoncuje hru nebo ne
+     * @return false protoze command neukoncuje hru
+     */
     @Override
     public boolean exit() {
         return false;
