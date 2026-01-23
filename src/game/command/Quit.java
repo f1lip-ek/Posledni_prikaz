81a6c08d954f9414f7af0e9dc13e26201a2c108a
Filip <filipzitek@seznam.cz>
2026-01-23 09:50:50 +0100

    adding javaDocs to Quit.java

--- src/game/command/Quit.java
+++ src/game/command/Quit.java
@@ -1,6 +1,15 @@
 package game.command;
 
+/**
+ * Trida pro command ktery ukoncuje hru
+ */
 public class Quit implements Command{
+
+    /**
+     * Command ktery ukonci celou hru
+     * @param text nic nedela
+     * @return text ktery presvedci uzivatele ze se hra ukoncila
+     */
     @Override
     public String execute(String text) {
         return "Hra se ukoncuje.";
