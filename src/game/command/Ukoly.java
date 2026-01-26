1a1f9617393b72a0748b159c3eb8ade35e25f2fe
filip <filipzitek@seznam.cz>
2026-01-26 12:52:09 +0100

    adding command Ukoly.java

--- src/game/command/Ukoly.java
+++ src/game/command/Ukoly.java
@@ -1,10 +1,26 @@
 package game.command;
 
+import com.google.gson.Gson;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.Arrays;
+
 public class Ukoly implements Command{
 
     @Override
     public String execute(String text) {
-        return "ukoly";
+        Gson gson = new Gson();
+        try (Reader reader = new FileReader("resources/ukoly.json")) {
+            String[] pomoc = gson.fromJson(reader, String[].class);
+            return Arrays.toString(pomoc);
+        } catch (FileNotFoundException ex){
+            System.err.println(ex.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException("Chyba při načítání JSON: " + e.getMessage());
+        }
+        return null;
     }
 
     @Override
