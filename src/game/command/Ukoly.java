7e3f441231e7fe4c8b562d62b47219a2a0794844
Filip <filipzitek@seznam.cz>
2026-02-05 16:55:09 +0100

    adding method getSpravnyRadek to get quests which are done in blue

--- src/game/command/Ukoly.java
+++ src/game/command/Ukoly.java
@@ -1,19 +1,29 @@
 package game.command;
 
 import com.google.gson.Gson;
-
-import java.io.FileNotFoundException;
+import game.postavy.Entita;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.Arrays;
 
 public class Ukoly implements Command{
 
+    private Entita hrac;
+
+    public Ukoly(Entita hrac) {
+        this.hrac = hrac;
+    }
+
+    public Ukoly(){}
+
     @Override
     public String execute(String text) {
         Gson gson = new Gson();
         try (Reader reader = new FileReader("resources/ukoly.json")) {
             String[] pomoc = gson.fromJson(reader, String[].class);
+            for (int i = 0; i < hrac.getCisloUkolu(); i++) {
+                pomoc[i] = getSpravnyRadek(pomoc[i]);
+            }
             return Arrays.toString(pomoc);
         }catch (Exception e) {
             throw new RuntimeException("Chyba při načítání JSON: " + e.getMessage());
@@ -23,5 +33,9 @@ public class Ukoly implements Command{
     @Override
     public boolean exit() {
         return false;
+    }
+
+    public String getSpravnyRadek(String text){
+        return "\u001B[34m" + text + "\u001B[0m";
     }
 }
