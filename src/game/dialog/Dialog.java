8cf59b297d76030d707f44b0f7f1e4e2f106dfd7
Filip <filipzitek@seznam.cz>
2026-02-10 18:10:32 +0100

    adding javaDocs

--- src/game/dialog/Dialog.java
+++ src/game/dialog/Dialog.java
@@ -6,15 +6,20 @@ import java.io.FileReader;
 import java.io.Reader;
 import java.util.ArrayList;
 
+/**
+ * Trida pro manipulovani s dialogy
+ */
 public class Dialog {
 
     private ArrayList textyDoKonzole;
-    private int aktualniIndex = 0;
 
     public Dialog(){
         nacistList();
     }
 
+    /**
+     * Metoda ktera nacte vsechny dialogy do ArrayListy
+     */
     private void nacistList(){
         Gson gson = new Gson();
         try (Reader reader = new FileReader("resources/vypisyDoKonzole.json")) {
@@ -24,6 +29,12 @@ public class Dialog {
         }
     }
 
+    /**
+     * Metoda ktera vrati urcity dialog
+     * @param cislo cislo dialogu
+     * @param text nepouzivan
+     * @return dany dialog
+     */
     public String getDialog(int cislo, String text){
         return textyDoKonzole.get(cislo) + "\n";
     }
