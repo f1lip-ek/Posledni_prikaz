a170a6900bc26629dc0f129e6a15b1f1a879e7ca
Filip <filipzitek@seznam.cz>
2026-02-12 19:39:21 +0100

    adding javaDocs

--- src/game/HraData.java
+++ src/game/HraData.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
  * Reprezentuje herní data načtená ze souboru JSON. Tato třída slouží jako
  * datový kontejner pro veškerý statický obsah hry, jako jsou předměty,
  * postavy, lokace a úkoly.
+ * @author Michaela Meitnarova
  */
 public class HraData {
 
